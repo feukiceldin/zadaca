f456089ab2d859a599e9e2ce5867fe9f60c98415
Eldin Feukic <feukiceldin@gmail.com>
2022-01-16 15:38:41 +0100

    Sve zadace dosad

---
+++ java-introduction/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Moje ime je Eldin Feukić.");
+        System.out.println("Rođen sam 14.12.2005. i imam 16 godina.");
+        System.out.println("Pohađam srednju elektrotehničku školu u Tuzli,");
+        System.out.println("Kao i školu programiranja \"Java Academy\" Gornja Tuzla.");
+        System.out.println("Slobodno vrijeme provodim igrajući igrice sa prijateljima. \n");
+        System.out.println("Fun fact: U jednoj godini, Java se preuzme oko milijardu puta.");
+    }
+}
