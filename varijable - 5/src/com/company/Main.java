98396cca4bfdfe5e3ff53e09abc7a36e358d33c2
Eldin Feukic <feukiceldin@gmail.com>
2022-01-20 12:26:28 +0100

    Uradjena Java zadaca za varijable

---
+++ varijable - 5/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double cube1Side = 5; //Cube 1
+        double cube2Side = 7; //Cube 2
+
+        System.out.println("Area of cube 1 is " + (cube1Side * cube1Side) + " and the volume is " + (cube1Side * cube1Side * cube1Side) + "\n");
+        System.out.println("Area of cube 2 is " + (cube2Side * cube2Side) + " and the volume is " + (cube2Side * cube2Side * cube2Side));
+
+    }
+}
