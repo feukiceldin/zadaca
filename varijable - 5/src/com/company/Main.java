5dd42e50deb8a7545b6b47fc78bd1f2dd360a4b9
feukiceldin <64590850+feukiceldin@users.noreply.github.com>
2022-01-22 15:57:09 +0100

    Ispravljena zadaća 5

--- varijable - 5/src/com/company/Main.java
+++ varijable - 5/src/com/company/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        double cube1Side = 5; //Cube 1
-        double cube2Side = 7; //Cube 2
+        double cube1Side = 5.2; //Cube 1
+        double cube2Side = 7.3; //Cube 2
 
-        System.out.println("Area of cube 1 is " + (cube1Side * cube1Side) + " and the volume is " + (cube1Side * cube1Side * cube1Side) + "\n");
-        System.out.println("Area of cube 2 is " + (cube2Side * cube2Side) + " and the volume is " + (cube2Side * cube2Side * cube2Side));
+        System.out.println("Area of cube 1 is " + (6*cube1Side * cube1Side) + " and the volume is " + (cube1Side * cube1Side * cube1Side) + "\n");
+        System.out.println("Area of cube 2 is " + (6*cube2Side * cube2Side) + " and the volume is " + (cube2Side * cube2Side * cube2Side));
 
     }
 }
